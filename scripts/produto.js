2eb94c680a28f656f0bc32913fdec25ed7b6b4ed
Paulo Diego <paulo.diego@unip.br>
2023-02-19 14:47:24 -0300

    Tabela produtos

--- scripts/produto.js
+++ scripts/produto.js
@@ -58,6 +58,18 @@ async function GetBuscarProdutos(params) {
     GeradorCard(listaProdutos);
 }
 
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
+
 
 
 
